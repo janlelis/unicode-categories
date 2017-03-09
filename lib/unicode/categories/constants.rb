6f8e0427f998414a3bc9dadeb9550e2cb9613b0a
Jan Lelis <mail@janlelis.de>
2017-03-09 16:22:38 +0100

    Release v1.1.2

--- lib/unicode/categories/constants.rb
+++ lib/unicode/categories/constants.rb
@@ -1,6 +1,6 @@
 module Unicode
   module Categories
-    VERSION = "1.1.1".freeze
+    VERSION = "1.1.2".freeze
     UNICODE_VERSION = "9.0.0".freeze
     DATA_DIRECTORY = File.expand_path(File.dirname(__FILE__) + '/../../../data/').freeze
     INDEX_FILENAME = (DATA_DIRECTORY + '/categories.marshal.gz').freeze
