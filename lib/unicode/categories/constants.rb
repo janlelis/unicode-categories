cdb24ee06caf205ff84050f8c001b1df87302f9a
Jan Lelis <mail@janlelis.de>
2018-06-01 10:27:42 +0200

    Unicode 11

--- lib/unicode/categories/constants.rb
+++ lib/unicode/categories/constants.rb
@@ -1,7 +1,7 @@
 module Unicode
   module Categories
     VERSION = "1.2.2".freeze
-    UNICODE_VERSION = "10.0.0".freeze
+    UNICODE_VERSION = "11.0.0".freeze
     DATA_DIRECTORY = File.expand_path(File.dirname(__FILE__) + '/../../../data/').freeze
     INDEX_FILENAME = (DATA_DIRECTORY + '/categories.marshal.gz').freeze
   end
