54d7f9bfacdaee90f205c7c37f6ecc2115b581d2
Jan Lelis <mail@janlelis.de>
2019-05-08 08:48:39 +0200

    Unicode 12.1

--- lib/unicode/categories/constants.rb
+++ lib/unicode/categories/constants.rb
@@ -3,7 +3,7 @@
 module Unicode
   module Categories
     VERSION = "1.4.0"
-    UNICODE_VERSION = "12.0.0"
+    UNICODE_VERSION = "12.1.0"
     DATA_DIRECTORY = File.expand_path(File.dirname(__FILE__) + "/../../../data/").freeze
     INDEX_FILENAME = (DATA_DIRECTORY + "/categories.marshal.gz").freeze
   end
