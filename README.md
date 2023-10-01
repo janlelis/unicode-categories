d430a9b6b0044c709977103d7ce27a2fe83fc20a
Jan Lelis <hi@ruby.consulting>
2023-10-01 14:32:15 +0200

    Update Unicode version number

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Returns a list which [General Categories](https://en.wikipedia.org/wiki/Unicode_character_property#General_Category) a Unicode string belongs to.
 
-Unicode version: **15.0.0** (September 2022)
+Unicode version: **15.1.0** (September 2023)
 
 Supported Rubies: **3.1**, **3.0**, **2.7**
 
--- lib/unicode/categories/constants.rb
+++ lib/unicode/categories/constants.rb
@@ -3,7 +3,7 @@
 module Unicode
   module Categories
     VERSION = "1.8.0"
-    UNICODE_VERSION = "15.0.0"
+    UNICODE_VERSION = "15.1.0"
     DATA_DIRECTORY = File.expand_path(File.dirname(__FILE__) + "/../../../data/").freeze
     INDEX_FILENAME = (DATA_DIRECTORY + "/categories.marshal.gz").freeze
   end
