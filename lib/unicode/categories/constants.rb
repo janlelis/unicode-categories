a639c68ac38ee3c4466b1646dcae86fe2bcfe70e
Jan Lelis <hi@syntaxtransformation.com>
2026-09-19 16:35:10 +0200

    Release v1.12.0

--- lib/unicode/categories/constants.rb
+++ lib/unicode/categories/constants.rb
@@ -2,7 +2,7 @@
 
 module Unicode
   module Categories
-    VERSION = "1.11.0"
+    VERSION = "1.12.0"
     UNICODE_VERSION = "18.0.0"
     DATA_DIRECTORY = File.expand_path(File.dirname(__FILE__) + "/../../../data/").freeze
     INDEX_FILENAME = (DATA_DIRECTORY + "/categories.marshal.gz").freeze
